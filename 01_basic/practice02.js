1781fa6f73d7e0d9ed667fe5e913a1ee3be4d0a9
tiwaripraveen <tiwaripraveen.2006@gmail.com>
2026-09-10 23:25:23 +0530

    array

---
+++ 01_basic/practice02.js
@@ -0,0 +1,17 @@
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+console.log(fruits[0]);
+console.log(fruits[3]);
+console.log(fruits.length);
+let numbers = [10, 20, 30, 40, 50];
+numbers.splice(2,1,100)
+
+console.log(numbers );
+let arr = [1, 2, 3];
+arr.push(4);
+arr.unshift(0)
+console.log(arr)
+let arr1 = [10, 20, 30, 40, 50];
+arr1.splice(0,1);
+//console.log(arr1)
+arr1.splice(4,1);
+console.log(arr1)
